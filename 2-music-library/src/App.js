0f3ccaab4f444d5aabc37d52897a13015bfd855f
OLATUNDE <olatundequadril@gmail.com>
2022-03-24 18:00:52 -0400

    rr-5-expand-applications-with-react-router-dom

---
+++ 2-music-library/src/App.js
@@ -0,0 +1,60 @@
+import { useState, useRef, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Gallery from './component/Gallery';
+import SearchBar from './component/SearchBar';
+import { DataContext } from './context/DataContext';
+import {SearchContext} from './context/SearchContext'
+import ArtistView from './component/ArtistView'
+import AlbumView from './component/AlbumView'
+
+function App() {
+  
+  let [message, setMessage] = useState("Search for Music!");
+  let [data, setData] = useState([]);
+  let searchInput = useRef('');
+
+  const API_URL = 'https://itunes.apple.com/search?term=';
+
+  const handleSearch = (e, term)=>{
+    e.preventDefault()
+    const fetchData = async () => {
+      document.title = `${term} Music`;
+      const response = await fetch(API_URL + term);
+      const resData = await response.json();
+      if(resData.results.length>0){
+         return setData(resData.results)}
+        else{
+         return setMessage("Not found")
+        }
+    }
+    fetchData()
+  }
+
+  return (
+    <div>
+      {message}
+      <Router>
+          <Routes>
+                <Route path='/'element={
+                  <Fragment>
+                    <SearchContext.Provider value={{
+                      term: searchInput,
+                      handleSearch: handleSearch
+                    }}>
+                      <SearchBar />
+                    </SearchContext.Provider>
+                    <DataContext.Provider value={data}>
+                      <Gallery data={data}/>
+                    </DataContext.Provider>
+              </Fragment>
+              }/>
+                   <Route path='/album/:id' element={<AlbumView/>}/>
+                    <Route path='/artist/:id' element={<ArtistView/>}/>
+              
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
